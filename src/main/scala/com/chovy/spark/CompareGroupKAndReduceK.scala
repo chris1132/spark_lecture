815a7333f58f88b181c02068c9dcc34e4c3db73c
chris1132 <793224504@qq.com>
2018-09-14 16:31:36 +0800

    reduceByKey mapValues

--- src/main/scala/com/chovy/spark/CompareGroupKAndReduceK.scala
+++ src/main/scala/com/chovy/spark/CompareGroupKAndReduceK.scala
@@ -10,15 +10,27 @@ object CompareGroupKAndReduceK {
 
     val sc = new SparkContext(conf)
 
-    val words = Array("one","two","three","four","fize")
+    val words = Array("one","two","three","four","one")
 
-    val wordRDD = sc.parallelize(words).map((_,1))
+    val wordRDD = sc.parallelize(words).map((_,2))
 
     val wordReduce = wordRDD.reduceByKey(_+_).foreach(println)
 
-    println("------------------------------")
+    println("1------------------------------")
 
     val wordGroup = wordRDD.groupByKey().map(e=>(e._1,e._2.sum)).foreach(println)
+
+    println("2------------------------------")
+
+    val wordSort = wordRDD.reduceByKey((a,b)=>a+1).sortByKey(false).foreach(println)
+
+    println("3------------------------------")
+
+    val wordSortByKey = wordRDD.reduceByKey(_+_).sortBy(_._2,false).foreach(println)
+
+    println("4------------------------------")
+
+    val mapValue = wordRDD.mapValues(x=>x+2).foreach(println)
 
   }
 
